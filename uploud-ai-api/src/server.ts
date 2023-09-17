31e3f40e1bd53a1e65a8de7b52c1c339865e7cc1
Lucas Moura <123328537+EuLucasMoura@users.noreply.github.com>
2023-09-17 09:13:35 -0300

    Finalizando o front-end e Iniciando as rotas do back-end

---
+++ uploud-ai-api/src/server.ts
@@ -0,0 +1,12 @@
+import { fastify } from 'fastify'
+import { prisma } from './lib/prisma'
+import { getAllPromptsRoutes } from './routes/get-all-prompts'
+const app = fastify()
+
+app.register(getAllPromptsRoutes)
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('🚀 HTTP Server Running on localhost:3333 ! 🚀')
+})
