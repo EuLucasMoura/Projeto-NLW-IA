2fc89acb2fb4d42fee9649183f2a23f54010ac66
Lucas Moura <123328537+EuLucasMoura@users.noreply.github.com>
2023-09-17 18:32:02 -0300

    Finalizando Back-end e iniciando integração

--- uploud-ai-api/src/routes/uploud-video.ts
+++ uploud-ai-api/src/routes/uploud-video.ts
@@ -3,6 +3,12 @@ import { prisma } from "../lib/prisma"
 import { fastifyMultipart } from "@fastify/multipart"
 import path from "node:path"
 import { randomUUID } from "node:crypto"
+import { BusboyFileStream } from "@fastify/busboy"
+import fs from "node:fs"
+import { pipeline } from "node:stream"
+import { promisify } from "node:util"
+
+const pump = promisify(pipeline)
 
 export async function uploudVideoRoute(app: FastifyInstance) {
     app.register(fastifyMultipart, {
@@ -23,10 +29,22 @@ export async function uploudVideoRoute(app: FastifyInstance) {
             return reply.status(400).send({error: 'Invalid input type, please upload a MP3.'})
         }
 
-        const fileBaseName = path.basename(data.fieldname, extension)
-
+        const fileBaseName = path.basename(data.filename, extension)
         const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`
-    
+
+        const uploadDestination = path.resolve(__dirname, '../../tmp', fileUploadName)
+
+        await pump(data.file, fs.createWriteStream(uploadDestination))
+
+        const video = await prisma.video.create({
+            data: {
+                name: data.filename,
+                path: uploadDestination
+            }
+        })
+
+        return {
+            video,
+        }
     })
 }
-
--- uploud-ai-api/src/server.ts
+++ uploud-ai-api/src/server.ts
@@ -1,9 +1,21 @@
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
 import { prisma } from './lib/prisma'
 import { getAllPromptsRoutes } from './routes/get-all-prompts'
+import { uploudVideoRoute } from './routes/uploud-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateIACompletionRoute } from './routes/generate-ia-completion'
+
 const app = fastify()
 
+app.register(fastifyCors, {
+    origin: '*',
+})
+
 app.register(getAllPromptsRoutes)
+app.register(uploudVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateIACompletionRoute)
 
 app.listen({
     port: 3333,
